4889df5791bd240a6d60f7361a7ea2210df0f96b
patboll <123997750+patboll@users.noreply.github.com>
2023-07-23 17:03:14 -1000

    Update email_practice.py

--- email_practice.py
+++ email_practice.py
@@ -8,14 +8,29 @@ import random
 messages = pd.read_csv(r"C:\Users\Patrick\PycharmProjects\Scrapping\ChatGPT_messages_2.csv")
 df3 = pd.read_csv(r"C:\Users\Patrick\PycharmProjects\Scrapping\contact_list_4.csv")
 
-# for i in range(2):
-for i in range(len(df3['Name'])):
+for i in range(1):
+# for i in range(len(df3['Name'])):
     outlook = win32.Dispatch('outlook.application')
     mail = outlook.CreateItem(0)
+
+    # I found the next 10 lines of code on stackoverflow, I don't understand it
+    # But this is what sets the address the e-mail is sent from.
+    # The e-mail needs to be part of your outlook account.
+    From = None
+    for myEmailAddress in outlook.Session.Accounts:
+        if "patrick@datatouches.com" in str(myEmailAddress):
+            From = myEmailAddress
+            break
+
+    if From != None:
+        # This line basically calls the "mail.SendUsingAccount = xyz@email.com" outlook VBA command
+        mail._oleobj_.Invoke(*(64209, 0, 8, 0, From))
+        
+
     # Change the "to" address to the column to loop thru
     # ******** NEXT LINE NEEDS TO BE CHANGED *****
-    mail.To = df3['Email'].values[i]
-    # mail.To = 'patricklboll@gmail.com'
+    # mail.To = df3['Email'].values[i]
+    mail.To = 'patricklboll@gmail.com'
     mail.Subject = 'Power BI Consulting Services from DataTouches'
     mail.GetInspector
 
